7638e39a34bab690af1f5b899947b6a0233b8cf5
Waleed Ahmed <waleed301195@hotmail.com>
2017-10-18 00:59:23 +0200

    Updated detail action method to use action results

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,16 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic book controller";
+            
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                return new RedirectResult("/");
+            }
+
+            return Content("Hello from the comic book controller");
+            
         }
     }
 }
